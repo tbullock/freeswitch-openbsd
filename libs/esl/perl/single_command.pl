bc87e49a638ced8eac295a92e7604c03b51c309b
Ted Bullock <tbullock@comlore.com>
2014-11-03 16:40:55 -0700

    Now that we've ported libesl, we don't need this

--- libs/esl/perl/single_command.pl
+++
@@ -1,8 +0,0 @@
-require ESL;
-
-my $command = shift;
-my $args = join(" ", @ARGV);
-
-my $con = new ESL::ESLconnection("localhost", "8021", "ClueCon");
-my $e = $con->api($command, $args);
-print $e->getBody();
