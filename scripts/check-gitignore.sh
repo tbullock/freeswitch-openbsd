1bf0ab5ba2e1750dc0ffcf8e9eb7a7d593cb007c
Ted Bullock <tbullock@comlore.com>
2014-11-27 21:47:17 -0700

    Delete all the scripts that won't be of any use.
    For now, I'll leave the lua and perl script folders in place

--- scripts/check-gitignore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## files we might want to ignore
-git ls-files -o --exclude-standard | sed -e "s:.:\/\\0:"
-
-## files we are accidentally ignoring
-git ls-files -i --exclude-standard | sed -e "s:.:\!\/\\0:"
-
